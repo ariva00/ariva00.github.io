4689337cec0fdea87606f924c68df5767b4cc65d
ariva00 <38658922+ariva00@users.noreply.github.com>
2025-06-11 08:22:17 +0000

    Deploying to gh-pages from @ ariva00/ariva00.github.io@1fbfdf368841f5cb6be7c3e5842e40057bfec3a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is my CV, including my education, work experience, and skills.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
